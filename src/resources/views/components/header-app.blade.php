2ca885d6f3e855aaf26e13c7d8d37326f4e16731
takayashiki <07rena01ichika@gmail.com>
2025-12-10 01:02:43 +0900

    SVGロゴを一括化

--- src/resources/views/components/header-app.blade.php
+++ src/resources/views/components/header-app.blade.php
@@ -1,6 +1,13 @@
 <header class="header-app">
     <div class="header-inner">
-        <h1 class="header-logo"><a href="{{ route('products.index') }}">COACHTECH</a></h1>
+
+        {{-- 未認証と同じ SVG ロゴをそのまま流用 --}}
+        <h1 class="header-logo">
+            <a href="{{ route('products.index') }}">
+               @include('components.logo-svg')
+            </a>
+        </h1>
+
         <nav class="header-nav">
             <ul class="header-list">
                 <li><a href="{{ route('products.create') }}" class="header-link">出品する</a></li>
@@ -8,10 +15,11 @@
                 <li>
                     <form method="POST" action="{{ route('logout') }}" class="logout-form">
                         @csrf
-                        <button type="submit" class="header-link logout-btn">ログアウト</button>
+                        <button type="submit" class="logout-btn">ログアウト</button>
                     </form>
                 </li>
             </ul>
         </nav>
+
     </div>
 </header>
---
+++ src/resources/views/components/logo-svg.blade.php
@@ -0,0 +1,2 @@
+{{-- SVG ロゴ共通化 --}}
+{!! file_get_contents(public_path('images/logo.svg')) !!}
