97bdb828cf565d0118e13ad806e92cad620192f1
takayashiki <07rena01ichika@gmail.com>
2025-12-10 16:27:48 +0900

    メール認証画面完成

--- src/resources/views/auth/email-verify.blade.php
+++ src/resources/views/auth/email-verify.blade.php
@@ -1,12 +1,18 @@
-@extends('layouts.guest', ['css_path' => 'auth/email-verify.css'])
+@extends('layouts.guest')
 
 @section('title', 'メール認証')
+
+{{-- ★ CSSの読み込み方法を修正 ★ --}}
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/auth/email-verify.css') }}">
+@endsection
 
 @section('content')
 <div class="message-container">
     <p class="verification-message">登録していたメールアドレスに認証メールを送付しました。<br>メール認証を完了してください。</p>
 
-    <a href="{{ route('verification.verify', ['id' => 1, 'hash' => 'hash']) }}" class="verify-link-button">認証はこちらから</a>
+    {{-- ★認証リンクは、一時的に # を使用 ★ --}}
+    <a href="#" class="verify-link-button">認証はこちらから</a>
 
     <form method="POST" action="{{ route('verification.resend') }}" class="resend-form">
         @csrf
