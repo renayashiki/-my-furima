5fd4adf41ad01ef06dedf82da40309424bbf7e59
takayashiki <07rena01ichika@gmail.com>
2025-12-13 01:07:40 +0900

    商品一覧　途中

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -1,24 +1,65 @@
-@extends('layouts.app', ['css_path' => 'products/index.css'])
+@extends('layouts.app') 
 
 @section('title', '商品一覧')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/products/index.css') }}">
+@endsection
+
 @section('content')
-<div class="container">
-    <form action="{{ route('products.index') }}" method="GET" class="search-form">
-        <input type="text" name="keyword" placeholder="商品を検索" class="search-input">
-        <button type="submit" class="search-btn">検索</button>
-    </form>
-
-    <div class="tab-switch">
-        <a href="#" class="tab-link active">おすすめ</a>
-        <a href="#" class="tab-link">マイリスト</a>
-    </div>
-
-    <div class="product-list">
-        <a href="{{ route('products.show', ['id' => 1]) }}" class="product-item">
-            <img src="..." alt="商品画像" class="product-image">
-            <p class="product-price">¥10,000</p>
-        </a>
+<div class="main-content">
+    
+    <div class="content-wrapper">
+        
+        <div class="tab-navigation-area">
+            <div class="tab-switch">
+                
+                {{-- おすすめタブ (初期チェック状態) --}}
+                <input type="radio" id="tab-recommend" name="product-tabs" checked class="tab-radio">
+                <label for="tab-recommend" class="tab-link tab-recommend">
+                    おすすめ
+                </label>
+                
+                {{-- マイリストタブ --}}
+                <input type="radio" id="tab-mylist" name="product-tabs" class="tab-radio">
+                <label for="tab-mylist" class="tab-link tab-mylist">
+                    マイリスト
+                </label>
+            </div>
+            <div class="tab-divider"></div>
         </div>
+        
+        <div class="products-container">
+            
+            {{-- おすすめ商品リスト --}}
+            <div id="recommend-content" class="product-grid product-list">
+                @for ($i = 0; $i < 8; $i++) 
+                    <div class="product-item-wrapper">
+                        <a href="#" class="product-item">
+                            <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                            <div class="product-name-box"><p class="product-name-text">商品名</p></div>
+                        </a>
+                        <button class="favorite-btn"><i class="far fa-heart"></i></button>
+                    </div>
+                @endfor
+            </div>
+            
+            {{-- マイリスト商品リスト --}}
+            <div id="mylist-content" class="product-grid product-list">
+                 @for ($i = 0; $i < 2; $i++) 
+                    <div class="product-item-wrapper">
+                        <a href="#" class="product-item">
+                            <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                            <div class="product-name-box"><p class="product-name-text">商品名 (マイリスト)</p></div>
+                        </a>
+                        <button class="favorite-btn"><i class="far fa-heart"></i></button>
+                    </div>
+                @endfor
+            </div>
+            
+        </div>
+        
+    </div>
+    
 </div>
 @endsection
