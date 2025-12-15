d8a2a81b2eef4b664068dd77416e81fe9402bfcf
takayashiki <07rena01ichika@gmail.com>
2025-12-15 15:51:31 +0900

    一覧画面のボタン機能へ変換

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -10,38 +10,45 @@
 <div class="main-content">
     
     {{-- 1. コントロール要素 (コンテンツ切り替え用。デザインから分離) --}}
+    {{-- checked属性で、デフォルトは「おすすめ」が選択された状態になります --}}
     <input type="radio" id="tab-recommend" name="product-tabs" checked class="tab-radio">
     <input type="radio" id="tab-mylist" name="product-tabs" class="tab-radio">
     
-    {{-- 2. デザイン要素 (タブボタンをデザインコンテナ内に戻す) --}}
+    {{-- 2. デザイン要素 (タブボタン) --}}
     <div class="tab-navigation-visual">
         <div class="tab-switch">
-            {{-- ラベルをデザインコンテナ内に入れ、フレックスで配置を制御 --}}
+            {{-- CSSハックのため、labelのfor属性とinputのid属性が一致していることを確認 --}}
             <label for="tab-recommend" class="tab-link tab-recommend">おすすめ</label>
             <label for="tab-mylist" class="tab-link tab-mylist">マイリスト</label>
         </div>
         <div class="tab-divider"></div>
     </div>
     
-    {{-- 3. コンテンツ（商品リスト）今後も修正必要機能入れる --}}
+    {{-- 3. コンテンツ（商品リスト） --}}
     <div class="products-container">
         
+        {{-- おすすめ商品リスト (デフォルト表示) --}}
         <div id="recommend-content" class="product-grid product-list">
+            {{-- DB連携前のダミーデータ (8個) --}}
             @for ($i = 0; $i < 8; $i++) 
                 <div class="product-item-wrapper">
                     <a href="#" class="product-item">
                         <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                        {{-- DBデータ挿入時に置き換える箇所 --}}
                         <div class="product-name-box"><p class="product-name-text">商品名</p></div>
                     </a>
                 </div>
             @endfor
         </div>
         
+        {{-- マイリスト商品リスト --}}
         <div id="mylist-content" class="product-grid product-list">
-             @for ($i = 0; $i < 2; $i++) 
+            {{-- DB連携前のダミーデータ (8個に統一) --}}
+            @for ($i = 0; $i < 8; $i++) 
                 <div class="product-item-wrapper">
                     <a href="#" class="product-item">
                         <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                        {{-- DBデータ挿入時に置き換える箇所 --}}
                         <div class="product-name-box"><p class="product-name-text">商品名 (マイリスト)</p></div>
                     </a>
                 </div>
