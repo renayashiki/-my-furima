113f46e28f1ebdc3f1fab720c0e4b7cbecd40697
takayashiki <07rena01ichika@gmail.com>
2025-12-15 11:12:51 +0900

    一時保存

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -9,54 +9,43 @@
 @section('content')
 <div class="main-content">
     
-    <div class="content-wrapper">
+    {{-- 1. コントロール要素 (コンテンツ切り替え用。デザインから分離) --}}
+    <input type="radio" id="tab-recommend" name="product-tabs" checked class="tab-radio">
+    <input type="radio" id="tab-mylist" name="product-tabs" class="tab-radio">
+    
+    {{-- 2. デザイン要素 (タブボタンをデザインコンテナ内に戻す) --}}
+    <div class="tab-navigation-visual">
+        <div class="tab-switch">
+            {{-- ラベルをデザインコンテナ内に入れ、フレックスで配置を制御 --}}
+            <label for="tab-recommend" class="tab-link tab-recommend">おすすめ</label>
+            <label for="tab-mylist" class="tab-link tab-mylist">マイリスト</label>
+        </div>
+        <div class="tab-divider"></div>
+    </div>
+    
+    {{-- 3. コンテンツ（商品リスト） --}}
+    <div class="products-container">
         
-        <div class="tab-navigation-area">
-            <div class="tab-switch">
-                
-                {{-- おすすめタブ (初期チェック状態) --}}
-                <input type="radio" id="tab-recommend" name="product-tabs" checked class="tab-radio">
-                <label for="tab-recommend" class="tab-link tab-recommend">
-                    おすすめ
-                </label>
-                
-                {{-- マイリストタブ --}}
-                <input type="radio" id="tab-mylist" name="product-tabs" class="tab-radio">
-                <label for="tab-mylist" class="tab-link tab-mylist">
-                    マイリスト
-                </label>
-            </div>
-            <div class="tab-divider"></div>
+        <div id="recommend-content" class="product-grid product-list">
+            @for ($i = 0; $i < 8; $i++) 
+                <div class="product-item-wrapper">
+                    <a href="#" class="product-item">
+                        <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                        <div class="product-name-box"><p class="product-name-text">商品名</p></div>
+                    </a>
+                </div>
+            @endfor
         </div>
         
-        <div class="products-container">
-            
-            {{-- おすすめ商品リスト --}}
-            <div id="recommend-content" class="product-grid product-list">
-                @for ($i = 0; $i < 8; $i++) 
-                    <div class="product-item-wrapper">
-                        <a href="#" class="product-item">
-                            <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
-                            <div class="product-name-box"><p class="product-name-text">商品名</p></div>
-                        </a>
-                        <button class="favorite-btn"><i class="far fa-heart"></i></button>
-                    </div>
-                @endfor
-            </div>
-            
-            {{-- マイリスト商品リスト --}}
-            <div id="mylist-content" class="product-grid product-list">
-                 @for ($i = 0; $i < 2; $i++) 
-                    <div class="product-item-wrapper">
-                        <a href="#" class="product-item">
-                            <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
-                            <div class="product-name-box"><p class="product-name-text">商品名 (マイリスト)</p></div>
-                        </a>
-                        <button class="favorite-btn"><i class="far fa-heart"></i></button>
-                    </div>
-                @endfor
-            </div>
-            
+        <div id="mylist-content" class="product-grid product-list">
+             @for ($i = 0; $i < 2; $i++) 
+                <div class="product-item-wrapper">
+                    <a href="#" class="product-item">
+                        <div class="product-image-box"><p class="product-image-text">商品画像</p></div>
+                        <div class="product-name-box"><p class="product-name-text">商品名 (マイリスト)</p></div>
+                    </a>
+                </div>
+            @endfor
         </div>
         
     </div>
